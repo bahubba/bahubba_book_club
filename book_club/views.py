1b2b50b6bfa57a0bbcfb1a4b12aed4aab37505d1
bahubba <bahubba89@gmail.com>
2023-05-14 11:36:47 -0400

    Added Signup and Login pages.  Added custom CSS file and favicons.  Added Raleway font (TODO - settle on this font?).

--- book_club/views.py
+++ book_club/views.py
@@ -1,3 +1,62 @@
+from django.db import IntegrityError
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+from .models import Reader
+
+
+def signup_user(req):
+    """
+    Reader (User) signup view and POST
+    """
+
+    # TODO - Probably need custom form...
+    return_dict = {'form': UserCreationForm()}
+
+    # Create user on POST
+    if req.method == 'POST':
+        if req.POST['password1'] == req.POST['password2']:
+            try:
+                # Create and persist the Reader (User)
+                reader = Reader.objects.create_user(
+                    req.POST['username'], req.POST['email'], req.POST['password1']
+                )
+                reader.save()
+                login(req, reader)
+
+                # TODO - Redirect to home page once implemented
+                # return redirect('home')
+            except IntegrityError:
+                return_dict['error'] = 'Username or email has already been taken'
+
+        else:
+            return_dict['error'] = 'Passwords do not match'
+
+    # Default to assuming GET functionality; Render the signup page
+    return render(req, 'book_club/signup_user.html', return_dict)
+
+
+def login_user(req):
+    """
+    Login view and POST
+    """
+
+    # TODO - Custom login form? Should be able to log in with username OR email
+    return_dict = {'form': AuthenticationForm()}
+
+    # Log in on POST
+    if req.method == 'POST':
+        reader = authenticate(
+            req, username=req.POST['username'], password=req.POST['password']
+        )
+        if reader is None:
+            return_dict['error'] = 'Username or password incorrect'
+        else:
+            login(req, reader)
+
+            # TODO - Redirect to home page once implemented
+            # return redirect('home')
+
+    # Default to assuming GET functionality; Render the login page
+    return render(req, 'book_club/login_user.html', return_dict)
