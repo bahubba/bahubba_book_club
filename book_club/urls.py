55528ab602636b93a2b494d307a71eccc4ff09c3
bahubba <bahubba89@gmail.com>
2023-05-22 21:30:36 -0400

    BAHubba-CU-85zt3kdvf: Manage Book Club Page
    Added Manage Book Club Page
    
    Stubbed form for updating book club details
    
    Stubbed manage readers page
    
    Stubbed manage preferences page
    
    Added ability to disband (soft delete only) book clubs

--- book_club/urls.py
+++ book_club/urls.py
@@ -9,4 +9,21 @@ urlpatterns = [
     path('create', views.create_book_club, name='create_book_club'),
     path('<str:book_club_name>/', views.book_club_home, name='book_club_home'),
     path('<str:book_club_name>/admin', views.book_club_admin, name='book_club_admin'),
+    path('<str:book_club_name>/admin/members', views.book_club_admin_members, name='book_club_admin_members'),
+    path('<str:book_club_name>/admin/prefs', views.book_club_admin_prefs, name='book_club_admin_prefs'),
+    path(
+        '<str:book_club_name>/admin/members/change-role',
+        views.book_club_admin_change_role,
+        name='book_club_admin_change_role'
+    ),
+    path(
+        '<str:book_club_name>/admin/members/remove',
+        views.book_club_admin_remove_reader,
+        name='book_club_admin_remove_reader'
+    ),
+    path(
+        '<str:book_club_name>/admin/disband',
+        views.book_club_admin_disband,
+        name='book_club_admin_disband'
+    ),
 ]
--- book_club/views.py
+++ book_club/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 from .models import BookClub, Reader
 from .forms import BookClubForm, ReaderCreationForm
@@ -89,7 +90,7 @@ def logout_reader(req):
 def home(req):
 
     # Pull the groups the reader is a member of
-    reader_clubs = BookClub.objects.filter(readers__id=req.user.id).all()
+    reader_clubs = BookClub.objects.filter(readers__id=req.user.id, disbanded__isnull=True).all()
     return_dict = {'book_clubs': reader_clubs, 'in_clubs': len(reader_clubs) > 0}
 
     return render(req, 'book_club/home.html', return_dict)
@@ -102,7 +103,7 @@ def book_clubs(req):
     """
 
     # Pull the groups the reader is a member of
-    reader_clubs = BookClub.objects.filter(readers__id=req.user.id).all()
+    reader_clubs = BookClub.objects.filter(readers__id=req.user.id, disbanded__isnull=True).all()
     # TODO - Is there a different way to do in_clubs' logic in the template?
     return_dict = {'book_clubs': reader_clubs, 'in_clubs': len(reader_clubs) > 0}
 
@@ -146,6 +147,7 @@ def book_club_home(req, book_club_name):
     try:
         book_club = BookClub.objects.get(
             name=book_club_name,
+            disbanded__isnull=True,
             readers__id=req.user.id,
             readers__bookclubreaders__left__isnull=True,
         )
@@ -168,21 +170,113 @@ def book_club_admin(req, book_club_name):
     """
 
     # Get the book club from the DB
+    book_club = __get_admin_club_or_none(book_club_name, req.user.id)
+
+    # If not an admin, redirect to home
+    # TODO - Redirect to current page
+    if book_club is None:
+        return redirect('home')
+
+    return render(
+        req,
+        'book_club/book_club_admin.html',
+        {'book_club': book_club, 'title': f'Manage {book_club_name} Details', 'section': 'details'},
+    )
+
+
+@login_required
+def book_club_admin_members(req, book_club_name):
+    """
+    Manage membership for book club
+    """
+
+    # Get the book club from the DB
+    book_club = __get_admin_club_or_none(book_club_name, req.user.id)
+
+    # If not an admin, redirect to home
+    # TODO - Redirect to current page
+    if book_club is None:
+        return redirect('home')
+
+    return render(
+        req,
+        'book_club/book_club_admin.html',
+        {
+            # TODO - filter readers to only those who haven't left
+            'book_club': book_club,
+            'title': f'Manage {book_club_name} Members',
+            'section': 'members',
+        },
+    )
+
+
+@login_required
+def book_club_admin_prefs(req, book_club_name):
+    """
+    Manage book club preferences
+    """
+
+    # Get the book club from the DB
+    book_club = __get_admin_club_or_none(book_club_name, req.user.id)
+
+    # If not an admin, redirect to home
+    # TODO - Redirect to current page
+    if book_club is None:
+        return redirect('home')
+
+    return render(
+        req,
+        'book_club/book_club_admin.html',
+        {
+            'book_club': book_club,
+            'title': f'Manage {book_club_name} Preferences',
+            'section': 'prefs',
+        }
+    )
+
+
+@login_required
+def book_club_admin_change_role(req, book_club_name):
+    pass
+
+
+@login_required
+def book_club_admin_remove_reader(req, book_club_name):
+    pass
+
+
+@login_required
+def book_club_admin_disband(req, book_club_name):
+    """
+    Disband a book club (soft delete)
+    """
+
+    # Get the book club from the DB
+    # TODO - Only creators should be able to disband groups
+    book_club = __get_admin_club_or_none(book_club_name, req.user.id)
+
+    # If not an admin, redirect to home
+    # TODO - Redirect to current page
+    if book_club is None:
+        return redirect('home')
+
+    # Set the book club to disbanded and redirect to home
+    book_club.disbanded = timezone.now()
+    book_club.save()
+
+    return redirect('home')
+
+
+def __get_admin_club_or_none(book_club_name, user_id):
     book_club: Optional[BookClub]
     try:
         book_club = BookClub.objects.get(
             name=book_club_name,
-            readers__id=req.user.id,
+            readers__id=user_id,
             readers__bookclubreaders__club_role='AD',
             readers__bookclubreaders__left__isnull=True,
         )
     except BookClub.DoesNotExist:
         book_club = None
 
-    # If not an admin, redirect to home
-    # TODO - Redirect to current page
-    print('book_club:', book_club)
-    if book_club is None:
-        return redirect('home')
-
-    return render(req, 'book_club/book_club_admin.html', {'book_club': book_club, 'title': f'Manage {book_club_name}'})
+    return book_club
